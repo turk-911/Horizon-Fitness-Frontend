92fb33e9515c3b56f84c4efd24a953a5ebbc196e
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-07-09 12:53:52 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://horizon-fitness-backend.onrender.com/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
